e57908e138ef9692a23a651bba4affc88da1ff67
abaniuszewicz <abaniuszewicz@gmail.com>
2021-03-23 17:51:35 +0100

    Adding initial project structure

---
+++ RBToolsContextMenu/RBToolsContextMenu.UI/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RBToolsContextMenu.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ RBToolsContextMenu/RBToolsContextMenu.UI/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace RBToolsContextMenu.UI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
