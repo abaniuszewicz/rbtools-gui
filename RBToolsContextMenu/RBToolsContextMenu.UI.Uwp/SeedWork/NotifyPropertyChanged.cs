785f13c9a3ab853674e96e8259b9c149b82e7bd1
abaniuszewicz <abaniuszewicz@gmail.com>
2021-03-28 09:51:34 +0200

    Adding 'RBToolsContextMenu.UI.Uwp' project

---
+++ RBToolsContextMenu/RBToolsContextMenu.UI.Uwp/SeedWork/NotifyPropertyChanged.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using RBToolsContextMenu.UI.Uwp.Annotations;
+
+namespace RBToolsContextMenu.UI.Uwp.SeedWork
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
